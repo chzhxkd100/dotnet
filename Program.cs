fcd4ed73a3fe12e01ca1b1e3b88a00d81468abcf
chzhxkd100 <chzhxkd100@naber.com>
2024-11-18 18:33:06 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("whatisyourname");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}, at {currentDate:t}");
+        Console.WriteLine($"{Environment.NewLine}Press Any key to exit...");
+        Console.ReadKey(true);
+    }
+}
